a80011d1231f560b6a549ce0a585585cced6e6de
Dan Honey <dan@localhost.localdomain>
2014-08-04 18:31:38 -0400

    stuff

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,6 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
-
 (require 'multi-term)
 (setq multi-term-program "/bin/bash")
 (setq multi-term-program-switches "--login")
@@ -8,15 +7,42 @@
 (require 'linum)
 (global-linum-mode 1)
 
+;;auto reload
+(global-auto-revert-mode t)
 
 (global-set-key "\C-x\C-m" 'execute-extended-command)
 (global-set-key "\C-c\C-m" 'execute-extended-command)
 
 ;; Themes
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes/emacs-color-theme-solarized/")
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/monokai/")
 ;(load-theme 'solarized-light t)
-(load-theme 'solarized-dark t)
+;(load-theme 'solarized-dark t)
 
 ;; Fonts
 ;(custom-set-faces'(default ((t (:inherit nil :height 100 :family "DejaVu Sans Mono")))))
 (custom-set-faces'(default ((t ( "-*-Inconsolata-normal-normal-normal-*-20-*-*-*-m-0-iso10646-1")))))
+
+
+;; Custom toggle between light/dark theme with F11 binding
+(defcustom default-light-color-theme 'solarized-light
+  "default light theme")
+
+(defcustom default-dark-color-theme 'monokai
+  "default dark theme")
+
+(defun toggle-dark-light-theme ()
+  (interactive)
+
+  (let ((is-light (find default-light-color-theme custom-enabled-themes)))
+    (dolist (theme custom-enabled-themes)
+      (disable-theme theme))
+    (load-theme (if is-light default-dark-color-theme default-light-color-theme))))
+
+(if window-system
+    (progn
+      (global-set-key (kbd "<f11>") 'toggle-dark-light-theme)
+      (load-theme 'solarized-light)
+      (set-face-font 'default (if (eq window-system 'w32)
+                                  "Consolas-10" "Liberation Mono-10"))))
+
